d1effe763555334e067f130dabb634e39ddcd04e
shanmathi <shanmathiemail@gmail.com>
2022-07-01 18:55:35 +0530

    added git ignore file

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Card, Table, ToastContainer } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  InputGroup,
+  Table,
+  ToastContainer,
+} from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import Navbar from "../Navbar/Navbar";
@@ -9,10 +15,22 @@ const Home = () => {
   const params = useParams();
   const token = localStorage.getItem("token");
   const [student, setStudent] = useState([]);
+  const [academicYear, setAcademicYear] = useState([]);
+  const [academicYearApi, setAcademicYearApi] = useState([]);
 
+  const getInitialState = () => {
+    const value = "please select the year";
+    return value;
+  };
+
+  const [value, setValue] = useState(getInitialState);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   const fetchStudent = () => {
     axios
-      .get(`http://3.110.131.173:4000/api/v1/studentProfile/${params.id}`, {
+      .get(`http://3.110.131.173:4000/api/v1/year`, {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
@@ -20,16 +38,53 @@ const Home = () => {
         console.log(response.data);
       });
   };
+  const fetchStudentByYear = () => {
+    axios
+      .post(`http://3.110.131.173:4000/api/v1/autoSearch`, {
+        headers: { Authorization: `Bearer ${token}` },
+        academic_year: academicYear,
+      })
+      .then((response) => {
+        setAcademicYearApi(response.data.data);
+        console.log(response.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchStudentByYear();
+  }, [academicYear]);
   useEffect(() => {
     fetchStudent();
   }, []);
+  console.log(academicYearApi, "academicYearApi");
+
   return (
     <div>
       <Navbar></Navbar>
-      <div class="container">
+      <div className="container">
         <Card style={{ width: "200", marginTop: "4%" }}>
           <Card.Header>List of students</Card.Header>
           <Card.Body>
+            <select
+              value={value}
+              // onChange={
+              //   }
+              onChange={(e) => {
+                setAcademicYear(e.target.options[e.target.selectedIndex].text);
+                // handleGradeFilter(gradeSectionList, e.target.value);
+                // setAcademicYear(e.target.value)
+              }}
+            >
+              <option value="none">Select the Year</option>
+              {student.map((data) => {
+                // {console.log(data.academic_year)}
+
+                return (
+                  <option value={data.year_id}>{data.academic_year}</option>
+                );
+              })}
+            </select>
+
             <Link to="/Create">
               <Button variant="primary">add</Button>
             </Link>
@@ -50,22 +105,13 @@ const Home = () => {
                   <th scope="col">Student Type</th>
                 </tr>
               </thead>
-
-              <tbody>
-                <tr>
-                  <th scope="row"></th>
-                  <td>{student.student_name}</td>
-                  <td>{student.DOB}</td>
-                  <td>{student.gender}</td>
-                  <td>{student.email}</td>
-                  <td>{student.admission_date}</td>
-                  <td>{student.address}</td>
-                  <td>{student.phone_number}</td>
-                  <td>{student.admission_no}</td>
-                  <td>{student.year_id}</td>
-                  <td>{student.student_type}</td>
-                </tr>
-              </tbody>
+              {Object.keys(academicYearApi).map((key) => {
+                return (
+                  <div key={key}>
+                    <h1>{key}</h1>
+                  </div>
+                );
+              })}
             </Table>
           </Card.Body>
         </Card>
